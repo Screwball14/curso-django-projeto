5d8f0b385c116a8094e2b86e716ca82d498681a4
Victor Alex <victoralexvidal@gmail.com>
2024-03-11 14:45:08 -0300

    refactor.alter pagination.py

--- utils/pagination.py
+++ utils/pagination.py
@@ -1,13 +1,32 @@
 import math
 
+
 def make_pagination_range(qty_pages_shown, current_page, pg_range=[]): 
     #pagination_range = o quanto de paginas mostrara ao
     #usuario, qty_pages = a quantidade de paginas, current_pag = a pagina em que o usuario está
-    # if current_page >= 3:
-    #     return [3,4,5,6]
-    middle = math.ceil(qty_pages_shown/2)
+    
+    #current_page é 1
+    middle = math.ceil(qty_pages_shown/2) # se é 4 então middle é *2*
+    start = current_page - middle # *-1*
+    stop = current_page + middle # *3*
 
-    start = current_page - middle
-    stop = current_page + middle
+    start_offset = abs(start) if start < 0 else 0
 
-    return pg_range[start:stop]
+    total_pages = len(pg_range)
+
+    if start < 0:
+        start = 0
+        stop += start_offset
+
+    
+    if stop > total_pages:
+        start -= abs(total_pages - stop) # serve para diminuir o start para o stop ser menor com a diferenca entre stop e total_pages
+
+
+    if current_page > total_pages:
+        raise ValueError()
+        
+        
+    
+
+    return pg_range[start:stop] 
--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -3,9 +3,24 @@ from utils.pagination import make_pagination_range
 
 class PaginationTest(TestCase):
     def test_pagination_makes_right_range(self):
-        pagination = make_pagination_range(pg_range=20, qty_pages_shown=list(range(0,6)), current_page=1)
-        self.assertEqual(pagination, [1,2,3,4])
+        pagination = make_pagination_range(pg_range=list(range(1,21)), qty_pages_shown=4, current_page=1)
+        self.assertEqual([1,2,3,4], pagination) 
+
+        pagination = make_pagination_range(pg_range=list(range(1,21)), qty_pages_shown=4, current_page=2)
+        self.assertEqual([1,2,3,4], pagination)
+
 
     def test_page_range_advences_paginationnumber(self):
-        pagination = make_pagination_range(pg_range=20, qty_pages_shown=list(range(0,6)), current_page=3)
-        self.assertEqual(pagination, [3,4,5,6])
+        pagination = make_pagination_range(pg_range=list(range(1,21)), qty_pages_shown=4, current_page=3)
+        self.assertEqual([2,3,4,5], pagination)
+
+        pagination = make_pagination_range(pg_range=list(range(1,21)), qty_pages_shown=4, current_page=4)
+        self.assertEqual([3,4,5,6], pagination )
+
+    def test_page_does_not_passes_range(self):
+        pagination = make_pagination_range(pg_range=list(range(1,21)), qty_pages_shown=4, current_page=20)
+        self.assertEqual([17,18,19,20], pagination)
+
+    def test_page_throws_error_if_exceeded(self):
+        with self.assertRaises(ValueError):
+            make_pagination_range(qty_pages_shown=4, current_page=21, pg_range=list(range(1,21)))
